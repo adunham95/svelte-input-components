a8f0d6dec103b954a233e4fcb7dc41826e51ee2f
Adrian <adunham95@gmail.com>
2025-01-03 09:52:13 -0500

    Added document page

---
+++ src/lib/Components/Sidebar.svelte
@@ -0,0 +1,94 @@
+<script lang="ts">
+	let expanded = $state('');
+
+	function toggleExpanded(name: string) {
+		if (expanded === name) {
+			expanded = '';
+		} else {
+			expanded = name;
+		}
+	}
+
+	$inspect(expanded);
+
+	const links = [
+		{ name: 'Dashboard', href: '/' },
+		{
+			name: 'Teams',
+			id: 'teams',
+			links: [
+				{ name: 'Engineering', href: '/engineering' },
+				{ name: 'Human Resources', href: '/hr' }
+			]
+		},
+		{ name: 'Contacts', href: '/contacts' },
+		{
+			name: 'Projects',
+			id: 'projects',
+			links: [
+				{ name: 'GraphQL API', href: '/graph-api' },
+				{ name: 'iOS App', href: '/ios' }
+			]
+		},
+		{ name: 'Calender', href: '/calender' }
+	];
+</script>
+
+<ul role="list" class="-mx-2 space-y-1">
+	{#each links as link}
+		{#if link.href}
+			<li>
+				<a
+					href={link.href}
+					class="block rounded-md py-2 pl-10 pr-2 text-sm/6 font-semibold text-gray-700 hover:bg-gray-50"
+				>
+					{link.name}
+				</a>
+			</li>
+		{/if}
+		{#if link.id}
+			<li>
+				<div>
+					<button
+						type="button"
+						class="flex w-full items-center gap-x-3 rounded-md p-2 text-left text-sm/6 font-semibold text-gray-700 hover:bg-gray-50"
+						aria-controls="sub-menu-1"
+						aria-expanded="false"
+						onclick={() => toggleExpanded(link.id)}
+					>
+						<!-- Expanded: "rotate-90 text-gray-500", Collapsed: "text-gray-400" -->
+						<svg
+							class={`size-5 shrink-0 ${link.id === expanded ? 'rotate-90 text-gray-500' : 'text-gray-400'}`}
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+							data-slot="icon"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z"
+								clip-rule="evenodd"
+							></path>
+						</svg>
+						{link.name}
+					</button>
+					<!-- Expandable link section, show/hide based on state. -->
+					<ul class={`mt-1 px-2 ${link.id === expanded ? 'block' : 'hidden'}`} id="sub-menu-1">
+						{#if link.links}
+							{#each link.links as sublink}
+								<li>
+									<a
+										href={sublink.href}
+										class="block rounded-md py-2 pl-9 pr-2 text-sm/6 text-gray-700 hover:bg-gray-50"
+									>
+										{sublink.name}
+									</a>
+								</li>
+							{/each}
+						{/if}
+					</ul>
+				</div>
+			</li>
+		{/if}
+	{/each}
+</ul>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,100 @@
 <script lang="ts">
 	import '../app.css';
+	import Sidebar from '$lib/Components/Sidebar.svelte';
 	let { children } = $props();
+
+	let mobileNavOpen = $state(false);
+
+	function toggleNav() {
+		mobileNavOpen = !mobileNavOpen;
+	}
+
+	$inspect(mobileNavOpen);
 </script>
 
-{@render children()}
+<div>
+	<div
+		class={`relative z-50 lg:hidden ${mobileNavOpen ? 'hidden' : 'block'}`}
+		role="dialog"
+		aria-modal="true"
+	>
+		<div class="fixed inset-0 bg-gray-900/80" aria-hidden="true"></div>
+
+		<div class="fixed inset-0 flex">
+			<div
+				class={`relative mr-16 flex w-full max-w-xs flex-1 ${mobileNavOpen ? '-translate-x-full' : 'translate-x-0'}`}
+			>
+				<div class="absolute left-full top-0 flex w-16 justify-center pt-5">
+					<button type="button" class="-m-2.5 p-2.5" onclick={toggleNav}>
+						<span class="sr-only">Close sidebar</span>
+						<svg
+							class="size-6 text-white"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							aria-hidden="true"
+							data-slot="icon"
+						>
+							<path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12"></path>
+						</svg>
+					</button>
+				</div>
+
+				<!-- Sidebar component, swap this element with another sidebar if you like -->
+				<div class="flex grow flex-col gap-y-5 overflow-y-auto bg-white px-6 pb-2">
+					<div class="flex h-16 shrink-0 items-center">
+						<p>Svelte Input Components</p>
+					</div>
+					<nav class="flex flex-1 flex-col">
+						<Sidebar></Sidebar>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Static sidebar for desktop -->
+	<div class="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+		<!-- Sidebar component, swap this element with another sidebar if you like -->
+		<div class="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
+			<div class="flex h-16 shrink-0 items-center">
+				<p>Svelte Input Components</p>
+			</div>
+			<nav class="flex flex-1 flex-col">
+				<Sidebar></Sidebar>
+			</nav>
+		</div>
+	</div>
+
+	<div
+		class="sticky top-0 z-40 flex items-center gap-x-6 bg-white px-4 py-4 shadow-sm sm:px-6 lg:hidden"
+	>
+		<button type="button" class="-m-2.5 p-2.5 text-gray-700 lg:hidden" onclick={toggleNav}>
+			<span class="sr-only">Open sidebar</span>
+			<svg
+				class="size-6"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				aria-hidden="true"
+				data-slot="icon"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+				></path>
+			</svg>
+		</button>
+		<div class="flex-1 text-sm/6 font-semibold text-gray-900">Svelte Input Components</div>
+	</div>
+
+	<main class="py-10 lg:pl-72">
+		<div class="px-4 sm:px-6 lg:px-8">
+			<!-- Your content -->
+			{@render children()}
+		</div>
+	</main>
+</div>
